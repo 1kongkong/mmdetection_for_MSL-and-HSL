04ad8eb9736e68ddc909ff4092ca59489da4b1ff
1kongkong <bisifu14@163.com>
2023-07-21 18:42:08 +0800

    update pointnet2 ssg for titan

--- configs/pointnet2/pointnet2_msg_2xb16-cosine-80e_titan-seg.py
+++ configs/pointnet2/pointnet2_msg_2xb16-cosine-80e_titan-seg.py
@@ -41,34 +41,9 @@ train_pipeline = [
 # model settings
 model = dict(
     backbone=dict(
-        _delete_=True,
-        type='PointNet2SAMSG',
-        in_channels=9,  # [xyz, rgb], should be modified with dataset
-        num_classes=7,
-        ignore_index=7,
+        in_channels=9,  # [xyz, rgb, normalized_xyz]
         num_points=(12500, 3125, 780, 195),
-        radii=((0.05, 0.1), (0.1, 0.2), (0.2, 0.4), (0.4, 0.8)),
-        num_samples=((16, 32), (16, 32), (16, 32), (16, 32)),
-        sa_channels=(((16, 16, 32), (32, 32, 64)), ((64, 64, 128), (64, 96,
-                                                                    128)),
-                     ((128, 196, 256), (128, 196, 256)), ((256, 256, 512),
-                                                          (256, 384, 512))),
-        aggregation_channels=(None, None, None, None),
-        fps_mods=(('D-FPS'), ('D-FPS'), ('D-FPS'), ('D-FPS')),
-        fps_sample_range_lists=((-1), (-1), (-1), (-1)),
-        dilated_group=(False, False, False, False),
-        out_indices=(0, 1, 2, 3),
-        sa_cfg=dict(
-            type='PointSAModuleMSG',
-            pool_mod='max',
-            use_xyz=True,
-            normalize_xyz=False)),
-    decode_head=dict(
-        fp_channels=((1536, 256, 256), (512, 256, 256), (352, 256, 128),
-                     (128, 128, 128, 128))))
-
-model = dict(
-    backbone=dict(in_channels=9),  # [xyz, rgb, normalized_xyz]
+    ),
     decode_head=dict(
         num_classes=7,
         ignore_index=7,
