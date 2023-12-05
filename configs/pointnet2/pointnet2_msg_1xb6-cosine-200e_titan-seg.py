fe88e4008ce6830bbc559d66f6a11fde71252984
1kongkong <bisifu14@163.com>
2023-12-05 11:52:26 +0800

    add pointnet2 model config for titan

---
+++ configs/pointnet2/pointnet2_msg_1xb6-cosine-200e_titan-seg.py
@@ -0,0 +1,101 @@
+_base_ = [
+    "../_base_/datasets/titan-seg.py",
+    "../_base_/models/pointnet2_msg.py",
+    "../_base_/schedules/seg-cosine-200e.py",
+    "../_base_/default_runtime.py",
+]
+
+# dataset settings
+class_names = ("Impervious Ground", "Grass", "Building", "Tree", "Car", "Power Line", "Bare land")
+
+num_points = 50000
+block_size = 50
+backend_args = None
+train_pipeline = [
+    dict(
+        type="LoadPointsFromFile",
+        coord_type="DEPTH",
+        shift_height=False,
+        use_color=True,
+        load_dim=6,  # 原数据的列数
+        use_dim=[0, 1, 2, 3, 4, 5],  # 保留的列
+        backend_args=backend_args,
+    ),
+    dict(
+        type="LoadAnnotations3D",
+        with_bbox_3d=False,
+        with_label_3d=False,
+        with_mask_3d=False,
+        with_seg_3d=True,
+        backend_args=backend_args,
+    ),
+    dict(type="PointSegClassMapping"),
+    dict(
+        type="IndoorPatchPointSample",
+        num_points=num_points,
+        block_size=block_size,
+        ignore_index=len(class_names),
+        use_normalized_coord=True,
+        enlarge_size=0.2,
+        min_unique_num=None,
+    ),
+    dict(
+        type="RandomFlip3D",
+        sync_2d=False,
+        flip_ratio_bev_horizontal=0.5,
+        flip_ratio_bev_vertical=1e-10,
+        flip_box3d=False,
+    ),
+    dict(type="GlobalRotScaleTrans", rot_range=[-0.78539816, 0.78539816], scale_ratio_range=[0.95, 1.05]),
+    dict(
+        type="RandomJitterPoints",
+    ),
+    # dict(type='NormalizePointsColor', color_mean=None),
+    dict(type="Pack3DDetInputs", keys=["points", "pts_semantic_mask"]),
+]
+
+# model settings
+model = dict(
+    backbone=dict(
+        in_channels=9,  # [xyz, rgb, normalized_xyz]
+        num_points=(12500, 3125, 780, 195),
+    ),
+    decode_head=dict(
+        num_classes=7, ignore_index=7, loss_decode=dict(class_weight=None)
+    ),  # S3DIS doesn't use class_weight
+    test_cfg=dict(
+        num_points=num_points,
+        block_size=block_size,
+        sample_rate=0.5,
+        use_normalized_coord=True,
+        batch_size=6,
+    ),
+)
+
+# data settings
+train_dataloader = dict(
+    batch_size=6,
+    num_workers=6,
+    dataset=dict(pipeline=train_pipeline),
+)
+
+# runtime settings
+default_hooks = dict(
+    checkpoint=dict(
+        type="CheckpointHook",
+        interval=5,
+        by_epoch=True,
+        save_best=["acc", "miou"],
+        rule="greater",
+    )
+)
+
+# 混合精度训练
+optim_wrapper = dict(
+    type="AmpOptimWrapper",
+    loss_scale="dynamic",
+    optimizer=dict(type="Adam", lr=0.001, weight_decay=0.001),
+    clip_grad=None,
+)
+
+train_cfg = dict(by_epoch=True, max_epochs=200, val_interval=5)
