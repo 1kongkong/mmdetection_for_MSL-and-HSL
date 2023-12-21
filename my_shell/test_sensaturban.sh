702f6c49875c6f86b2a57ce6c72313c4f3d3f266
1kongkong <bisifu14@163.com>
2023-12-21 16:59:02 +0800

    add offline_test & draw_seg

--- my_shell/test_sensaturban.sh
+++ my_shell/test_sensaturban.sh
@@ -5,10 +5,10 @@ export PYTHONPATH=$PYTHONPATH:$workdir
 cd $workdir
 
 pcd_path=data/SensatUrban/test
-# work_dirs/dual_kpfcnn_1xb6-cosine-100e_sensaturban-seg/20231203_103749/dual_kpfcnn_1xb6-cosine-100e_sensaturban-seg/best_miou_epoch_54.pth
+# work_dirs/dual_kpfcnn_1xb6-cosine-100e_sensaturban-seg/20231213_111041/dual_kpfcnn_1xb6-cosine-100e_sensaturban-seg/best_miou_epoch_62.pth
 model_type=dual_kpfcnn_1xb6-cosine-100e_sensaturban-seg
-train_date=20231203_103749
-model_pth=best_miou_epoch_54
+train_date=20231213_111041
+model_pth=best_miou_epoch_62
 
 work_dirs=work_dirs/${model_type}/${train_date}
 config=work_dirs/${model_type}/${model_type}.py
@@ -20,9 +20,12 @@ for i in {birmingham_block_2,birmingham_block_8,cambridge_block_15,cambridge_blo
 do 
     pcd=${pcd_path}/${i}.bin
     save_file=${work_dirs}/${i}
+    CUDA_VISIBLE_DEVICES=1 \
     python demo/pcd_seg_demo.py ${pcd} ${config} ${check_point} --out-dir ${save_file} --snapshot
 done
 
+python my_tools/offline_test.py --path ${work_dirs}
+python my_tools/draw_seg_mask.py --path ${work_dirs}
 
 
 # pcd=${pcd_path}/area_2.bin
--- my_shell/test_titan.sh
+++ my_shell/test_titan.sh
@@ -5,10 +5,10 @@ export PYTHONPATH=$PYTHONPATH:$workdir
 cd $workdir
 
 pcd_path=data/Titan/points
-# work_dirs/dgcnn_1xb6-cosine-100e_titan-seg/20231121_092442/dgcnn_1xb6-cosine-100e_titan-seg/best_miou_epoch_54.pth
-model_type=dgcnn_1xb6-cosine-100e_titan-seg
-train_date=20231121_092442
-model_pth=best_miou_epoch_54
+# work_dirs/dual_kpfcnn_1xb6-cosine-100e_titan-seg/20231220_200308/dual_kpfcnn_1xb6-cosine-100e_titan-seg/best_miou_epoch_80.pth
+model_type=dual_kpfcnn_1xb6-cosine-100e_titan-seg
+train_date=20231220_200308
+model_pth=best_miou_epoch_80
 
 work_dirs=work_dirs/${model_type}/${train_date}
 config=work_dirs/${model_type}/${model_type}.py
@@ -20,32 +20,9 @@ for i in {2,7,12,14,18,21,24,27}
 do 
     pcd=${pcd_path}/area_${i}.bin
     save_file=${work_dirs}/area_${i}
+    CUDA_VISIBLE_DEVICES=1 \
     python demo/pcd_seg_demo.py ${pcd} ${config} ${check_point} --out-dir ${save_file} --snapshot
 done
 
-
-
-# pcd=${pcd_path}/area_2.bin
-# save_file=${work_dirs}/area_2
-# python demo/pcd_seg_demo.py ${pcd} ${config} ${check_point} --out-dir ${save_file} --snapshot
-
-# pcd=${pcd_path}/area_7.bin
-# python demo/pcd_seg_demo.py ${pcd} ${config} ${check_point} --out-dir ${work_dirs} --snapshot
-
-# pcd=${pcd_path}/area_12.bin
-# python demo/pcd_seg_demo.py ${pcd} ${config} ${check_point} --out-dir ${work_dirs} --snapshot
-
-# pcd=${pcd_path}/area_14.bin
-# python demo/pcd_seg_demo.py ${pcd} ${config} ${check_point} --out-dir ${work_dirs} --snapshot
-
-# pcd=${pcd_path}/area_18.bin
-# python demo/pcd_seg_demo.py ${pcd} ${config} ${check_point} --out-dir ${work_dirs} --snapshot
-
-# pcd=${pcd_path}/area_21.bin
-# python demo/pcd_seg_demo.py ${pcd} ${config} ${check_point} --out-dir ${work_dirs} --snapshot
-
-# pcd=${pcd_path}/area_24.bin
-# python demo/pcd_seg_demo.py ${pcd} ${config} ${check_point} --out-dir ${work_dirs} --snapshot
-
-# pcd=${pcd_path}/area_27.bin
-# python demo/pcd_seg_demo.py ${pcd} ${config} ${check_point} --out-dir ${work_dirs} --snapshot
+python my_tools/offline_test.py --path ${work_dirs}
+python my_tools/draw_seg_mask.py --path ${work_dirs}
