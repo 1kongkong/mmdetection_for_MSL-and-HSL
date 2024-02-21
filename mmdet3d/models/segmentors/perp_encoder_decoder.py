8943a8a0d4ccce1503b7ff20f87687031b4c7e2d
1kongkong <bisifu14@163.com>
2024-02-21 11:38:33 +0800

    update predict func

--- mmdet3d/models/segmentors/perp_encoder_decoder.py
+++ mmdet3d/models/segmentors/perp_encoder_decoder.py
@@ -90,6 +90,10 @@ class PreP_EncoderDecoder3D(EncoderDecoder3D):
 
         loss_spe = self.prep.loss()
         if loss_spe is not None:
+            spe_loss_factor = self.train_cfg.get("spe_loss_factor", None)
+            if spe_loss_factor is not None:
+                for key in loss_spe.keys():
+                    loss_spe[key] = loss_spe[key] * spe_loss_factor
             losses.update(loss_spe)
 
         if self.with_auxiliary_head:
@@ -102,78 +106,80 @@ class PreP_EncoderDecoder3D(EncoderDecoder3D):
 
         return losses
 
-    def gridsample_slide_inference(self, point: Tensor, input_meta: dict) -> Tensor:
-        """Inference by gridsample and sliding-window with overlap.
-        Args:
-            point (Tensor): Input points of shape [N, 3+C].
-            input_meta (dict): Meta information of input sample.
+    # def gridsample_slide_inference(self, point: Tensor, input_meta: dict) -> Tensor:
+    #     """Inference by gridsample and sliding-window with overlap.
+    #     Args:
+    #         point (Tensor): Input points of shape [N, 3+C].
+    #         input_meta (dict): Meta information of input sample.
 
-        Returns:
-            Tensor: The output segmentation map of shape [num_classes, N].
-        """
-        if "titan" in input_meta["lidar_path"].lower():
-            grid_size = 0.4
-        elif "sensaturban" in input_meta["lidar_path"].lower():
-            grid_size = 0.2
-        else:
-            raise NotImplementedError
+    #     Returns:
+    #         Tensor: The output segmentation map of shape [num_classes, N].
+    #     """
+    #     if "titan" in input_meta["lidar_path"].lower():
+    #         grid_size = 0.4
+    #     elif "sensaturban" in input_meta["lidar_path"].lower():
+    #         grid_size = 0.2
+    #     else:
+    #         raise NotImplementedError
 
-        if self.test_cfg.get("voxel_size", None):
-            grid_size = self.test_cfg.voxel_size
-        print(f"grid_size:{grid_size}")
-        choice = self.voxel_sample(point[:, :3], grid_size)
-        # choice, _ = torch.sort(choice)
-        point_sub = point[choice, :].contiguous()
+    #     if self.test_cfg.get("voxel_size", None):
+    #         grid_size = self.test_cfg.voxel_size
+    #     print(f"grid_size:{grid_size}")
+    #     choice = self.voxel_sample(point[:, :3], grid_size)
+    #     # choice, _ = torch.sort(choice)
+    #     point_sub = point[choice, :].contiguous()
 
-        # 创建映射索引
-        tree = KDTree(point_sub[:, :3].cpu().numpy())
-        _, idx = tree.query(point[:, :3].cpu().numpy(), k=3, workers=4)
-        idx = torch.tensor(idx, device=point_sub.device, dtype=torch.long)
+    #     # 创建映射索引
+    #     tree = KDTree(point_sub[:, :3].cpu().numpy())
+    #     _, idx = tree.query(point[:, :3].cpu().numpy(), k=3, workers=4)
+    #     idx = torch.tensor(idx, device=point_sub.device, dtype=torch.long)
 
-        block_size = self.test_cfg.block_size
-        sample_rate = self.test_cfg.sample_rate
-        use_normalized_coord = self.test_cfg.use_normalized_coord
-        batch_size = self.test_cfg.batch_size
+    #     block_size = self.test_cfg.block_size
+    #     sample_rate = self.test_cfg.sample_rate
+    #     use_normalized_coord = self.test_cfg.use_normalized_coord
+    #     batch_size = self.test_cfg.batch_size
 
-        # patch_points is of shape [K*N, 3+C], patch_idxs is of shape [K*N]
-        patch_points, patch_idxs = self._sliding_whole_patch_generation(
-            point_sub, block_size, sample_rate, use_normalized_coord
-        )
-        for i in range(len(patch_points)):
-            channel_idx = torch.argsort(patch_points[i][:, 6])
-            patch_points[i] = patch_points[i][channel_idx]
-            patch_idxs[i] = patch_idxs[i][channel_idx]
+    #     # patch_points is of shape [K*N, 3+C], patch_idxs is of shape [K*N]
+    #     patch_points, patch_idxs = self._sliding_whole_patch_generation(
+    #         point_sub, block_size, sample_rate, use_normalized_coord
+    #     )
+    #     for i in range(len(patch_points)):
+    #         # 临时更改
+    #         channel_idx = torch.argsort(patch_points[i][:, 6])
+    #         # channel_idx = torch.argsort(patch_points[i][:, 9])
+    #         patch_points[i] = patch_points[i][channel_idx]
+    #         patch_idxs[i] = patch_idxs[i][channel_idx]
 
-        seg_logits = []  # save patch predictions
+    #     seg_logits = []  # save patch predictions
 
-        for batch_idx in range(0, len(patch_points), batch_size):
-            batch_points = patch_points[batch_idx : batch_idx + batch_size]
-            batch_seg_logit = self.encode_decode(
-                batch_points, [input_meta] * batch_size
-            )
-            batch_seg_logit = batch_seg_logit.transpose(1, 2).contiguous()
-            seg_logits.append(batch_seg_logit.view(-1, self.num_classes))
+    #     for batch_idx in range(0, len(patch_points), batch_size):
+    #         batch_points = patch_points[batch_idx : batch_idx + batch_size]
+    #         batch_seg_logit = self.encode_decode(
+    #             batch_points, [input_meta] * batch_size
+    #         )
+    #         batch_seg_logit = batch_seg_logit.transpose(1, 2).contiguous()
+    #         seg_logits.append(batch_seg_logit.view(-1, self.num_classes))
 
-        # aggregate per-point logits by indexing sum and dividing count
-        seg_logits = torch.cat(seg_logits, dim=0)  # [K*N, num_classes]
-        patch_idxs = torch.cat(patch_idxs)
-        expand_patch_idxs = patch_idxs.unsqueeze(1).repeat(1, self.num_classes)
-        preds_sub = point_sub.new_zeros(
-            (point_sub.shape[0], self.num_classes)
-        ).scatter_add_(dim=0, index=expand_patch_idxs, src=seg_logits)
-        count_mat = torch.bincount(patch_idxs)
-        preds_sub = preds_sub / count_mat[:, None]
+    #     # aggregate per-point logits by indexing sum and dividing count
+    #     seg_logits = torch.cat(seg_logits, dim=0)  # [K*N, num_classes]
+    #     patch_idxs = torch.cat(patch_idxs)
+    #     expand_patch_idxs = patch_idxs.unsqueeze(1).repeat(1, self.num_classes)
+    #     preds_sub = point_sub.new_zeros(
+    #         (point_sub.shape[0], self.num_classes)
+    #     ).scatter_add_(dim=0, index=expand_patch_idxs, src=seg_logits)
+    #     count_mat = torch.bincount(patch_idxs)
+    #     preds_sub = preds_sub / count_mat[:, None]
 
-        # 清理显存
-        del point, point_sub, patch_points, patch_idxs, expand_patch_idxs
-        torch.cuda.empty_cache()
+    #     # 清理显存
+    #     del point, point_sub, patch_points, patch_idxs, expand_patch_idxs
+    #     torch.cuda.empty_cache()
 
-        preds_sub = preds_sub.half()
-        preds = preds_sub[idx, :]
-        if len(preds.shape) == 3:
-            preds = torch.mean(preds, dim=-2)
+    #     preds_sub = preds_sub.half()
+    #     preds = preds_sub[idx, :]
+    #     if len(preds.shape) == 3:
+    #         preds = torch.mean(preds, dim=-2)
 
-        return preds.transpose(0, 1).contiguous()  # to [num_classes, K*N]
+    #     return preds.transpose(0, 1).contiguous()  # to [num_classes, K*N]
 
     def _forward(
         self, batch_inputs_dict: dict, batch_data_samples: OptSampleList = None
