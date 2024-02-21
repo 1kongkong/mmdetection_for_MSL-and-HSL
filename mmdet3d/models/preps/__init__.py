d100ca5cfddb670568c389d7adb80e5da566d135
1kongkong <bisifu14@163.com>
2024-02-21 11:36:19 +0800

    add kpconv like

--- mmdet3d/models/preps/__init__.py
+++ mmdet3d/models/preps/__init__.py
@@ -1,20 +1,20 @@
 from .cross_attention import (
     CrossInterpolatePreP,
     CrossInterpolatePreP2,
+    CrossInterpolatePreP3,
     CrossInterpolatePreP4,
-    CrossInterpolatePreP5,
     NNInterpolatePreP,
     IDWInterpolatePreP,
     BasePreP,
 )
-
+from .kpconv_like import KPConvPreP
 
 __all__ = [
     "CrossInterpolatePreP",
     "CrossInterpolatePreP2",
-    "CrossInterpolatePreP4",
-    "CrossInterpolatePreP5",
+    "CrossInterpolatePreP3",
     "NNInterpolatePreP",
     "IDWInterpolatePreP",
     "BasePreP",
+    "KPConvPreP",
 ]
